8996f4734dd661b7867968d32bdb6b00127f1541
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-03-27 04:48:56 +0700

    feat: change to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "hybrid",
   icon:"",
   adapter: cloudflare({
     runtime: {
