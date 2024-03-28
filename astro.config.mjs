25139f72f9e0263784bba6365482be886a83ab19
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-03-29 04:33:52 +0700

    feat: add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "static",
   integrations: [tailwind(), icon()],
+  site: 'https://bunnybunbun37204.github.io',
+  base: 'porkpork',
   image: {
     service: passthroughImageService(),
   },
