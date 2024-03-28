5157891d049ec29fe0dd364efd23bd7abc69a258
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-03-29 01:30:40 +0700

    fix: change to static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: "local",
-    },
-    imageService: 'passthrough'
-
-  }),
+  output: "static",
   integrations: [tailwind(), icon()],
 });
