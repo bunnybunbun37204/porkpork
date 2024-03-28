70a2d361d560d8da59814c33d022ba64ed828d9f
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-03-29 04:52:51 +0700

    fix: remove github

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "static",
   integrations: [tailwind(), icon()],
-  site: 'https://bunnybunbun37204.github.io',
-  base: 'porkpork',
   image: {
     service: passthroughImageService(),
   },
