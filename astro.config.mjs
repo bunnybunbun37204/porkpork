193b6ba42c4792b0060038ea6cd8212b3dbd2199
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-03-30 01:14:04 +0700

    feat: add more info

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig, sharpImageService } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
-import robotsTxt from "astro-robots-txt";
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://porkpork.pages.dev/",
-  integrations: [tailwind(), icon(), robotsTxt()],
+  integrations: [tailwind(), icon()],
   image: {
     service: sharpImageService(),
   },
