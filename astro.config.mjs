0074851c616eaa8adb12c1d8ab5fbb67412a9786
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-03-30 00:46:14 +0700

    fix: make link crawnable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind(), icon()],
+  site: "https://porkpork.pages.dev/",
+  integrations: [tailwind(), icon(), robotsTxt()],
   image: {
     service: sharpImageService(),
   },
